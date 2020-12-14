d5df1b139af9cdc62cb9ec87cdbccb175473c8c1
Giuse1 <48623997+Giuse1@users.noreply.github.com>
2020-12-14 14:16:21 +0100

    added cifar model

--- model.py
+++ model.py
@@ -42,5 +42,26 @@ class CNNMnist(nn.Module):
         return F.log_softmax(x, dim=1)
 
 
+class cifar(nn.Module):
+    def __init__(self, num_channels, num_classes):
+        super(CNNMnist, self).__init__()
+        self.conv1 = nn.Conv2d(num_channels, 32, kernel_size=3)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
+        self.fc1 = nn.Linear(1024, 64)
+        self.fc2 = nn.Linear(64, num_classes)
+
+    def forward(self, x):
+
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2(x), 2))
+        x = F.relu(F.max_pool2d(self.conv3(x), 2))
+
+        x = x.view(-1, x.shape[1]*x.shape[2]*x.shape[3])
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
 
 
